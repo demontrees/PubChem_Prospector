8a179b3c52e803493d150c35be65370496fa1960
demontrees <144124891+demontrees@users.noreply.github.com>
2024-05-09 20:03:04 -0600

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,13 @@ setup(
     url="https://github.com/demontrees/PubChem_Prospector",
     install_requires=[
         'urllib3>=1.26.16'
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
 )
