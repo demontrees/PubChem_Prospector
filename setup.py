c26d5e95275bc3fea153b80158e384349edcab44
demontrees <144124891+demontrees@users.noreply.github.com>
2024-05-10 15:39:38 -0600

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PubChem_Prospector",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     description="functions for bulk scraping pubchem annotations",
     author="demontrees",
